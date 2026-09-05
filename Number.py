58521b1f91405000d813fd135b572856a5bd421e
Aman Singh <as5645512@gmail.com>
2026-09-05 22:10:45 +0530

    number fix

--- Number.py
+++ Number.py
@@ -4,14 +4,12 @@
 #If n is even and in the inclusive range of 6 to 20, print Weird    
 #If n is even and greater than 20, print Not Weird
 
-if __name__ == '__main__':
-    n = int(input().strip())
-    
-    if n % 2 != 0:
-        print("Weird")
-    elif n % 2 == 0 and 2 <= n <= 5:
-        print("Not Weird")
-    elif n % 2 == 0 and 6 <= n <= 20:
-        print("Weird")
-    elif n % 2 == 0 and n > 20:
-        print("Not Weird")
+n=int(input("Enter an integer: "))
+if n % 2 != 0:
+    print("Weird")
+elif n % 2 == 0 and n in range(2, 6):
+    print("Not Weird")
+elif n % 2 == 0 and n in range(6, 21):
+    print("Weird")
+elif n % 2 == 0 and n > 20:
+    print("Not Weird")
