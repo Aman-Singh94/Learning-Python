1f72bb44786158eca0be564d12f278d95300a292
Aman Singh <as5645512@gmail.com>
2026-09-05 22:25:54 +0530

    feat: mathematical.py added

---
+++ mathematical.py
@@ -0,0 +1,10 @@
+#The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
+
+#The first line contains the sum of the two numbers.
+#The second line contains the difference of the two numbers (first - second).
+#The third line contains the product of the two numbers.
+a=int(input("Enter the first integer: "))
+b=int(input("Enter the second integer: "))
+print(f"The sum of {a} and {b} is: {a + b}")
+print(f"The difference of {a} and {b} is: {a - b}")
+print(f"The product of {a} and {b} is: {a * b}")
