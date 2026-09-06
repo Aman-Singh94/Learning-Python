43b428ec189084de21a7b4b8c12d48f6c4a19149
Aman Singh <as5645512@gmail.com>
2026-09-06 08:49:12 +0530

    feat: sequence

--- Sequence.py
+++ Sequence.py
@@ -1,5 +1,10 @@
 #Without using any string methods, try to print the following:
 #123....n
-
+n=int(input("Enter an integer: "))
+if 1 <= n <= 150:
+    for i in range(1, n+1):
+        print(i, end="")
+else:
+    print("Number is out of range. It should be between 1 and 150.")
 
 
