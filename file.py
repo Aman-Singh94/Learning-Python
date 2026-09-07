3022b2ef134b1b90bd0567b8b8f36e3495d505e3
Aman Singh <as5645512@gmail.com>
2026-09-07 22:16:12 +0530

    file close

--- file.py
+++ file.py
@@ -1,5 +1,7 @@
 read=open("file1.txt","r")
 print(read.read())  
+read.close()
+ #it is important to close the file after reading it to free up system resources.
 
 # We only read the file and print the content of the file, which is in the folder of python(main folder).
 #read1=open(r"C:\python's file\file1.txt","r")
