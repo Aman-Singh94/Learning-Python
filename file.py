a32aff6a080b12b914e4f3c816e4da502760a2c8
Aman Singh <as5645512@gmail.com>
2026-09-07 21:58:46 +0530

    feat: file.py

--- file.py
+++ file.py
@@ -1,1 +1,8 @@
-file=open("file1.txt","r")
+read=open("file1.txt","r")
+print(read.read())  
+
+# We only read the file and print the content of the file, which is in the folder of python(main folder).
+#read1=open(r"C:\python's file\file1.txt","r")
+#print(read.read())
+
+
