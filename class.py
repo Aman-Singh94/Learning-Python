b73c86444e390efbcb5198f7e6518b7c5a11b6a7
Aman Singh <as5645512@gmail.com>
2026-09-07 19:17:35 +0530

    feat: class

---
+++ class.py
@@ -0,0 +1,13 @@
+class student:
+    pass
+student1 = student()
+student2 = student()
+
+student1.name = "Aman"
+student1.marks = 85
+
+student2.name = "Rahul"
+student2.marks = 32
+
+print(student1.name + " " + str(student1.marks))
+print(student2.name, student2.marks)
