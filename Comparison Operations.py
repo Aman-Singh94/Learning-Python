78ae9b95d24f525e64d1abc47d48ec73bee32aa1
Aman Singh <as5645512@gmail.com>
2026-09-13 14:57:22 +0530

    feat:Comparison Operations

---
+++ Comparison Operations.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+z = np.array([10, 20, 30, 40, 50])
+
+print("Array:")
+print(z)
+
+print("\nGreater than 25:")
+print(z > 25)
+
+print("\nLess than 30:")
+print(z < 30)
+
+print("\nEqual to 30:")
+print(z == 30)
+
+print("\nNot equal to 30:")
+print(z != 30)
+
+print("\nGreater than or equal to 30:")
+print(z >= 30)
+
+print("\nLess than or equal to 30:")
+print(z <= 30)
