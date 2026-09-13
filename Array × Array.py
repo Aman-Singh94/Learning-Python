3c015a50dc523c845ed763922b4bdefea9d634dd
Aman Singh <as5645512@gmail.com>
2026-09-13 14:53:21 +0530

    feat:Array × Array

---
+++ Array × Array.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+z = np.array([10, 20, 30, 40])
+x = np.array([1, 2, 3, 4])
+
+print("Addition:")
+print(z + x)
+
+print("Subtraction:")
+print(z - x)
+
+print("Multiplication:")
+print(z * x)
+
+print("Division:")
+print(z / x)
+
